167773a8d2739d736ed298ef82fc51b902a76da7
Sam Kessler <sholomkessler1@gmail.com>
2021-05-07 21:03:01 -0400

    Imported persistent scene

---
+++ Assets/Scripts/DifficultySettingsSO.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "New Difficulty Settings", menuName = "Difficulty")]
+public class DifficultySettingsSO : ScriptableObject
+{
+    public int MaxEnemies;
+    [Range(1, 2)]
+    public float EnemyHealthIncrease;
+    [Range(1, 2)]
+    public float EnemyDamageIncrease;
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,182 @@
+﻿using System.Collections.Generic;
+using TMPro;
+#if !UNITY_WEBGL
+using UnityEditor;
+#endif
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Linq;
+using UnityEngine.InputSystem;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject settingsGO;
+    [SerializeField]
+    private Toggle muteToggle;
+    [SerializeField]
+    private Slider volumeSlider;
+    [SerializeField]
+    private Toggle movementToggle;
+    [SerializeField]
+    private TextMeshProUGUI volumeText;
+    [SerializeField]
+    private Button closeSettingsMenu;
+    [SerializeField]
+    private TMP_Dropdown DifficultyDropdown;
+    [SerializeField]
+    private GameObject LoadingScreen;
+    [SerializeField]
+    private DifficultySettingsSO[] Difficulties;
+    public DifficultySettingsSO SelectedDifficulty;
+    public static GameManager instance;
+    private int CurrentSceneIndex => SceneManager.GetActiveScene().buildIndex;
+    [SerializeField]
+    private Camera persistentCamera;
+
+    private void Awake()
+    {
+        instance = this;
+        DifficultyDropdown.onValueChanged.AddListener(SetDifficulty);
+#if !UNITY_EDITOR
+        LoadScene(1, new System.Action<AsyncOperation>[]
+        {
+            _ => SetLoadButtonListner(2),
+            _ => FindAndUpdateSettingsButton()
+        }, true);
+#endif
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+#if UNITY_EDITOR
+        SetLoadButtonListner(2);
+        FindAndUpdateSettingsButton();
+#else
+        updateSettings();
+#endif
+    }
+    private void Update()
+    {
+        InputSystem.Update();
+    }
+
+    public void updateSettings()
+    {
+        AudioListener.volume = muteToggle.isOn ? 0 : volumeSlider.value;
+        volumeText.text = $"Volume: {Mathf.RoundToInt(volumeSlider.value * 100)}/100";
+    }
+
+    public void SetDifficulty(int index)
+    {
+        SelectedDifficulty = Difficulties[index];
+    }
+    public void LoadScene(int index)
+    {
+        LoadScene(index, null, true);
+    }
+
+    public void LoadScene(int index, System.Action<AsyncOperation>[] onComplete, bool setActive)
+    {
+        LoadingScreen.SetActive(true);
+        persistentCamera.gameObject.SetActive(true);
+        if (CurrentSceneIndex != 0)
+            SceneManager.UnloadSceneAsync(CurrentSceneIndex);
+        AsyncOperation LoadSceneOperation = SceneManager.LoadSceneAsync(index, LoadSceneMode.Additive);
+        LoadSceneOperation.allowSceneActivation = setActive;
+        if (onComplete != null)
+        {
+            foreach (System.Action<AsyncOperation> operation in onComplete)
+            {
+                LoadSceneOperation.completed += operation;
+            }
+        }
+        LoadSceneOperation.completed += obj => SceneLoaded(index);
+    }
+
+    private void SceneLoaded(int index)
+    {
+        SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(index));
+        FindAndUpdateSettingsButton();
+        SetLoadButtonListner(2);
+        LoadingScreen.SetActive(false);
+        persistentCamera.gameObject.SetActive(false);
+    }
+
+    private void FindAndUpdateSettingsButton()
+    {
+        var buttonList = GetAllObjectsOnlyInSceneWithTag<Button>("SettingsButton");
+        if (buttonList.Count > 0)
+        {
+            Button settingsButton = buttonList[0];
+            settingsButton.onClick.RemoveAllListeners();
+            settingsButton.onClick.AddListener(() => settingsGO.SetActive(!settingsGO.activeInHierarchy));
+            var menu = GetAllObjectsOnlyInSceneWithTag<Transform>("Menu")?.FirstOrDefault();
+            if (menu != null)
+                closeSettingsMenu.onClick.AddListener(() => menu.gameObject.SetActive(true));
+        }
+        else
+        {
+            Debug.LogWarning("Could not find settings button in loaded scenes");
+        }
+        updateSettings();
+    }
+
+    private void SetLoadButtonListner(int index)
+    {
+        if (SceneManager.GetSceneByBuildIndex(1).isLoaded)
+        {
+            var buttonList = GetAllObjectsOnlyInSceneWithTag<Button>("LoadGameButton");
+            if (buttonList.Count > 0)
+            {
+                Button loadGameButton = buttonList[0];
+                loadGameButton.onClick.RemoveAllListeners();
+                loadGameButton.onClick.AddListener(() => LoadScene(index));
+            }
+            else
+            {
+                Debug.LogWarning("Could not find play button in loaded scenes");
+            }
+        }
+    }
+
+    public List<T> GetAllObjectsOnlyInScene<T>() where T : Object
+    {
+        List<T> objectsInScene = new List<T>();
+
+        foreach (T obj in Resources.FindObjectsOfTypeAll(typeof(T)) as T[])
+        {
+            if (
+#if !UNITY_WEBGL
+                !EditorUtility.IsPersistent(obj) && //If platform is webgl check if it is not an editor object
+#endif
+                !(obj.hideFlags == HideFlags.NotEditable || obj.hideFlags == HideFlags.HideAndDontSave))
+            {
+                objectsInScene.Add(obj);
+            }
+        }
+
+        return objectsInScene;
+    }
+
+    public List<T> GetAllObjectsOnlyInSceneWithTag<T>(string tag) where T : Component
+    {
+        List<T> objectsInScene = new List<T>();
+
+        foreach (T obj in Resources.FindObjectsOfTypeAll(typeof(T)) as T[])
+        {
+            if (
+#if !UNITY_WEBGL
+                !EditorUtility.IsPersistent(obj) && //If platform is webgl check if it is not an editor object
+#endif
+                !(obj.hideFlags == HideFlags.NotEditable || obj.hideFlags == HideFlags.HideAndDontSave) && obj.CompareTag(tag))
+            {
+                objectsInScene.Add(obj);
+            }
+        }
+
+        return objectsInScene;
+    }
+}
